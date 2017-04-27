d3357600853239497b1e2714975355d9a8856ccb
DANIEL THOMPSON <danny@DANIELs-MacBook-Air.local>
2017-04-26 18:09:59 -0700

    Radio updates and fixes including calendar webview.

---
+++ SSUMobile/Modules/Radio/Views/SSURadioCalendarViewController.h
@@ -0,0 +1,23 @@
+//
+//  UIViewController+SSURadioCalendarViewController.h
+//  SSUMobile
+//
+//  Created by DANIEL THOMPSON on 4/26/17.
+//  Copyright © 2017 Sonoma State University Department of Computer Science. All rights reserved.
+//
+
+/*
+    Interface for RadioCalendar's viewcontroller.
+    Purpose of the viewcontroller is to view the SSURadio calendar without simply adding
+    the webview to the radios subview.
+    Opens a new view and adds the webview to its subview for simpler navigation.
+ */
+
+@import UIKit;
+#import "SSURadioViewController.h"
+#import "SSURadioStreamer.h"
+
+
+@interface SSURadioCalendarViewController : UIViewController <SSURadioStreamerDelegate>
+- (void)viewDidLoad;
+@end
